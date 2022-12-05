bb1fa0fa7804d48e6d84675a861a509ba71924ff
= <=>
2022-12-05 00:36:50 -0300

    sirlene semana

---
+++ Sirlene/semana.js
@@ -0,0 +1,10 @@
+const read = require('readline-sync')
+
+let nome = (read.question("Qual o seu nome?: "))
+let nascimento = Number(read.question("Em que ano voce nasceu? :"))
+let atual = Number(read.question("Qual ano atual ?: "))
+
+let idade = (atual - nascimento)
+let semana = (idade * 521429)
+
+console.log(`Murilo voce tem ${idade} anos de idade e ${semana} semanas de vida`)
